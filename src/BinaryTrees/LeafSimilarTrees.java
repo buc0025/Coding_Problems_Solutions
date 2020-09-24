ddac95589035987f6c4ba46c1757cef58e05d463
buc0025 <stanley.yang001@gmail.com>
2020-09-24 15:01:27 -0400

    More efficient solution without using global variables

--- src/BinaryTrees/LeafSimilarTrees.java
+++ src/BinaryTrees/LeafSimilarTrees.java
@@ -7,6 +7,34 @@ Return true if and only if the two given trees with head nodes root1 and root2 a
  */
 
 public class LeafSimilarTrees {
+    //*********More efficient solution*************
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        // Creating a list for the leaves of both trees to compare in return statement;
+        List<Integer> tree1 = new ArrayList<>();
+        List<Integer> tree2 = new ArrayList<>();
+
+        sameLeaves(root1, tree1);
+        sameLeaves(root2, tree2);
+
+        return tree1.equals(tree2);
+    }
+
+    public List sameLeaves(TreeNode root, List list) {
+        if (root == null) {
+            return null;
+        }
+        // Adding to the list if value is a leaf
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+        }
+
+        sameLeaves(root.left, list);
+        sameLeaves(root.right, list);
+
+        return list;
+    }
+
+    //*********Original solution*************
     String leftTree = "";
     String rightTree = "";
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
