c6b9f2f4dfd261c8ac08d54de262776c8676de49
buc0025 <stanley.yang001@gmail.com>
2020-09-27 22:09:27 -0400

    Fixed null pointer exception and incompatible types

--- src/BinaryTrees/AllElementsBSTs.java
+++ src/BinaryTrees/AllElementsBSTs.java
@@ -11,46 +11,54 @@ public class AllElementsBSTs {
         List<Integer> tree1 = new ArrayList<>();
         List<Integer> tree2 = new ArrayList<>();
 
-        return combineLists(allNodes(root1, tree1), allNodes(root2, tree2));
+        // Combining the 2 lists after inorder traversal of the 2 trees
+        return combineLists(inOrder(root1, tree1), inOrder(root2, tree2));
     }
     // Method returns a sorted list of the combined lists from the two different trees
-    public List combineLists(List list1, List list2) {
+    public List combineLists(List<Integer> list1, List<Integer> list2) {
         List<Integer> combined = new ArrayList<>();
 
         int list1Index = 0; // keeps track of the index in list1;
         int list2Index = 0; // keeps track of the index in list2;
+
+        if (list1 == null && list2 != null) {
+            return list2;
+        }
+
+        if (list2 == null && list1 != null) {
+            return list1;
+        }
         // Adds smaller item between two lists to new list
         while (list1Index < list1.size() && list2Index < list2.size()) {
-            if ((int)list1.get(list1Index) < (int)list2.get(list2Index)) {
-                combined.add((int)list1.get(list1Index));
+            if (list1.get(list1Index) < list2.get(list2Index)) {
+                combined.add(list1.get(list1Index));
                 list1Index++;
             } else {
-                combined.add((int)list2.get(list2Index));
+                combined.add(list2.get(list2Index));
                 list2Index++;
             }
         }
         // Adds the remaining items in list1 if there's no more items left in list2
         while (list1Index < list1.size()) {
-            combined.add((int)list1.get(list1Index));
+            combined.add(list1.get(list1Index));
             list1Index++;
         }
         // Adds the remaining items in list2 if there's no more items left in list1
         while (list2Index < list2.size()) {
-            combined.add((int)list2.get(list2Index));
+            combined.add(list2.get(list2Index));
             list2Index++;
         }
         return combined;
     }
     // Method uses inorder traversal to get sort list from BST
-    public List allNodes(TreeNode root, List list) {
+    public List inOrder(TreeNode root, List list) {
         if (root == null) {
             return null;
         }
 
-        allNodes(root.left, list);
+        inOrder(root.left, list);
         list.add(root.val);
-
-        allNodes(root.right, list);
+        inOrder(root.right, list);
 
         return list;
     }
