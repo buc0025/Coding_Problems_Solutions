61fdb267dd04b0625e495e37f9e4c0bb52fc40d0
buc0025 <stanley.yang001@gmail.com>
2020-10-09 16:47:18 -0400

    Return number of switch flips required to form given string target

---
+++ src/strings/BulbSwitch.java
@@ -0,0 +1,34 @@
+package strings;
+/*
+https://leetcode.com/problems/bulb-switcher-iv/
+There is a room with n bulbs, numbered from 0 to n-1, arranged in a row from left to right. Initially all the bulbs are
+turned off. Your task is to obtain the configuration represented by target where target[i] is '1' if the i-th bulb is
+turned on and is '0' if it is turned off. You have a switch to flip the state of the bulb, a flip operation is defined
+as follows:
+
+Choose any bulb (index i) of your current configuration.
+Flip each bulb from index i to n-1.
+
+When any bulb is flipped it means that if it is 0 it changes to 1 and if it is 1 it changes to 0.
+Return the minimum number of flips required to form target.
+ */
+
+public class BulbSwitch {
+    public int minFlips(String target) {
+        int count = 0;
+
+        // If the first char is 1, switch has be turn on at least once and count will start at 1
+        if (target.indexOf("1") == 0) {
+            count = 1;
+        }
+
+        // Noticed that clusters revealed the number of switches. Any time a switch doesn't match
+        // with the switch next to it, it breaks the cluster
+        for (int i = 0; i < target.length() - 1; i++) {
+            if (target.charAt(i) != target.charAt(i + 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
