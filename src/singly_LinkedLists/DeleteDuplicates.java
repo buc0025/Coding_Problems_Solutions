2dd2f5cd550a545f8811eacbf356388c1009658c
buc0025 <stanley.yang001@gmail.com>
2020-10-16 12:48:27 -0400

    Remove all duplicates from Sorted Linked list so it only appears once

---
+++ src/singly_LinkedLists/DeleteDuplicates.java
@@ -0,0 +1,20 @@
+package singly_LinkedLists;
+/*
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+ */
+
+public class DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
