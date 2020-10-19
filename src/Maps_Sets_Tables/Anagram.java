871f101dc5041c45ccf62b3259844f169c40f343
buc0025 <stanley.yang001@gmail.com>
2020-10-18 23:06:53 -0400

    Improved runtime with new trick from Steven

--- src/Maps_Sets_Tables/Anagram.java
+++ src/Maps_Sets_Tables/Anagram.java
@@ -7,6 +7,26 @@ Given two strings s and t , write a function to determine if t is an anagram of 
 
 public class Anagram {
 
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            ++arr[s.charAt(i) - 'a'];
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            --arr[t.charAt(i) - 'a'];
+            if (arr[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public boolean isAnagram1(String s, String t) {
 
         Map<Character, Integer> map1 = new HashMap<>();
