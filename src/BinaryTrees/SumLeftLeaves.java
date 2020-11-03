ce8dd3d337faad18494876104c8268f0988178ac
buc0025 <stanley.yang001@gmail.com>
2020-11-02 21:01:13 -0500

    Redid tree problem without global variable

--- src/BinaryTrees/SumLeftLeaves.java
+++ src/BinaryTrees/SumLeftLeaves.java
@@ -11,6 +11,23 @@ public class SumLeftLeaves {
             return 0;
         }
 
+        int sum = 0;
+
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+
+        return sum;
+    }
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         int sum = 0;
