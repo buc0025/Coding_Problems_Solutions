72b50dec44664b078c2d13d0d256387d0c32cd63
buc0025 <stanley.yang001@gmail.com>
2020-11-03 13:03:12 -0500

    Incorrect solution to old linked list problem

--- src/singly_LinkedLists/DeleteDuplicates.java
+++ src/singly_LinkedLists/DeleteDuplicates.java
@@ -5,6 +5,18 @@ Given a sorted linked list, delete all duplicates such that each element appear 
  */
 
 public class DeleteDuplicates {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            }
+            current = current.next;
+        }
+        return head;
+    }
+
     public ListNode deleteDuplicates(ListNode head) {
         ListNode current = head;
 
