c92700c53f06a9be79a376e763cc0432c447873c
buc0025 <stanley.yang001@gmail.com>
2020-11-04 12:45:12 -0500

    Redid problem on removing values from Linked List

--- src/singly_LinkedLists/RemoveElements.java
+++ src/singly_LinkedLists/RemoveElements.java
@@ -8,6 +8,24 @@ Remove all elements from a linked list of integers that have value val.
 public class RemoveElements {
 
     public ListNode removeElements(ListNode head, int val) {
+        ListNode current = head;
+
+        // Checks and removes element after the first node
+        while (current != null && current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        // Checks first element
+        if (head != null && head.val == val) {
+            return head.next;
+        }
+        return head;
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
 
         ListNode current = head;
 
