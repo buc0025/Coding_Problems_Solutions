5f4f22d29e730ff99aed2bdc6b4fee6453d86f61
buc0025 <stanley.yang001@gmail.com>
2020-11-09 17:05:40 -0500

    Invert binary tree

--- src/BinaryTrees/InvertBinaryTree.java
+++ src/BinaryTrees/InvertBinaryTree.java
@@ -11,6 +11,25 @@ public class InvertBinaryTree {
             return null;
         }
 
+        // Swapping nodes
+        TreeNode left = root.right;
+        TreeNode right = root.left;
+
+        //Reassigning swapped nodes
+        root.right = right;
+        root.left = left;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
         TreeNode t1 = new TreeNode(root.val);
         t1.left = invertTree(root.right);
         t1.right = invertTree(root.left);
