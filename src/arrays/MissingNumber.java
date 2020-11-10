030dd1b554dbb0d8f7e78e28a705015a4c2c597e
buc0025 <stanley.yang001@gmail.com>
2020-11-10 10:57:47 -0500

    Brute force solution to missing number problem

---
+++ src/arrays/MissingNumber.java
@@ -0,0 +1,27 @@
+package arrays;
+/*
+https://leetcode.com/problems/missing-number/
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is
+missing from the array.
+ */
+
+public class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        // Created array that can hold elements 0 - n
+        int[] arr = new int[nums.length + 1];
+
+        // Fill the index with 1 if it's in the given nums array
+        for (int a : nums) {
+            ++arr[a];
+        }
+
+        // Index that is not filled is the missing number
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
