adf7b5d17b4da0033717c9df216454a4d72d1ea3
buc0025 <stanley.yang001@gmail.com>
2020-11-17 22:58:24 -0500

    Attempt at trimming a BST

--- src/BinaryTrees/TrimBST.java
+++ src/BinaryTrees/TrimBST.java
@@ -8,6 +8,39 @@ root of the trimmed binary search tree.
 
 public class TrimBST {
     //*****************Code below is an incorrect attempt**************
+    // Idea was to check if a node was lower than low or higher than high and then check if they have an children.
+    // If a node value was lower than low and had a right child that was greater than low, then that will become the new
+    // node. The same goes for left child of a node value that's higher than high but to check if it's lower than high
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < low) {
+            if (root.right != null && root.right.val >= low) {
+                root = root.right;
+            }
+            else if (root.left == null && root.right == null) {
+                root = null;
+            }
+        }
+
+        if (root.val > high) {
+            if (root.left == null && root.right == null) {
+                root = null;
+            }
+            if (root.left != null && root.left.val <= high) {
+                root = root.left;
+            }
+        }
+
+        trimBST(root.left, low, high);
+        trimBST(root.right, low, high);
+
+        return root;
+    }
+
+    //*****************Code below is an incorrect attempt**************
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) {
             return null;
