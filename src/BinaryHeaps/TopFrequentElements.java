ac03eb5c7a51ed1a933824f213f7fbd7a0d7c0b0
buc0025 <stanley.yang001@gmail.com>
2020-12-09 16:10:57 -0500

    Attempts at previous problems 12/9/2020

--- src/BinaryHeaps/TopFrequentElements.java
+++ src/BinaryHeaps/TopFrequentElements.java
@@ -6,6 +6,50 @@ Given a non-empty array of integers, return the k most frequent elements.
 
 public class TopFrequentElements {
 
+    //Incorrect attempt. Was under the assumption that top k frequent elements were unique and therefore used a set
+    public int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+
+        // Maps the number of occurrences as values
+        Map<Integer, Integer> map1 = new HashMap<>();
+
+        for (int n : nums) {
+            map1.put(n, map1.getOrDefault(n, 0) + 1);
+        }
+
+        // Maps the number of occurrences as keys
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for (int m : map1.keySet()) {
+            map2.put(map1.get(m), m);
+        }
+
+        // Set sorts and holds the number of occurrences of elements
+        Set<Integer> set = new HashSet<>();
+
+        for (int m : map2.keySet()) {
+            set.add(m);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int s : set) {
+            list.add(s);
+        }
+
+        int[] arr = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            int num = list.get(list.size()-1);
+            arr[i] = map2.get(num);
+            list.remove(list.size()-1);
+        }
+
+        return arr;
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
         int[] arr = new int[k];
 
--- src/arrays/KthFactor.java
+++ src/arrays/KthFactor.java
@@ -7,6 +7,21 @@ less than k factors.
  */
 
 public class KthFactor {
+
+    public int kthFactor(int n, int k) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                ++count;
+            }
+            if (count == k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public int kthFactor(int n, int k) {
         ArrayList<Integer> list = new ArrayList<>();
 
