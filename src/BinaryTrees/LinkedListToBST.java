013c05186965ef81d1055a5303ab8f877b9f3fe4
buc0025 <stanley.yang001@gmail.com>
2020-12-10 20:54:41 -0500

    Attempts at previous problems 12/10/2020

---
+++ src/BinaryTrees/LinkedListToBST.java
@@ -0,0 +1,47 @@
+package BinaryTrees;
+/*
+https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node
+never differ by more than 1.
+ */
+
+public class LinkedListToBST {
+
+    // Idea was taken from solution forming BST from sorted array
+    public TreeNode sortedListToBST(ListNode head) {
+        int[] arr = turnToArray(head);
+        return turnToBST(arr, 0, arr.length - 1);
+    }
+
+    public TreeNode turnToBST(int[] arr, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        // Treat like Binary search to assign node as middle
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(arr[mid]);
+        node.left = turnToBST(arr, left, mid - 1);
+        node.right = turnToBST(arr, mid + 1, right);
+
+        return node;
+    }
+
+    public int[] turnToArray(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        int[] arr = new int[list.size()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+
+        return arr;
+    }
+}
--- src/arrays/UnsortedSubarray.java
+++ src/arrays/UnsortedSubarray.java
@@ -8,6 +8,41 @@ You need to find the shortest such subarray and output its length.
  */
 
 public class UnsortedSubarray {
+
+    // Same exact solution as previous attempt from over 2 months ago
+    public int findUnsortedSubarray(int[] nums) {
+        int[] sorted = new int[nums.length];
+
+        for (int i = 0; i < sorted.length; i++) {
+            sorted[i] = nums[i];
+        }
+
+        Arrays.sort(sorted);
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != sorted[i]) {
+                start = i;
+                break;
+            }
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] != sorted[i]) {
+                end = i;
+                break;
+            }
+        }
+
+        if (start == end) {
+            return 0;
+        } else {
+            return end - start + 1;
+        }
+    }
+
     public int findUnsortedSubarray(int[] nums) {
         int[] clone = new int[nums.length];
         int index = 0;
