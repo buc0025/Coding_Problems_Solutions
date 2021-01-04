f06e32666c03c32f005eec7e800017a73269e080
buc0025 <stanley.yang001@gmail.com>
2021-01-03 21:37:24 -0500

    Attempt at mock interview problem 01/03/2021

---
+++ src/strings/PermutationInString.java
@@ -0,0 +1,36 @@
+package strings;
+/*
+https://leetcode.com/problems/permutation-in-string/
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the
+first string's permutations is the substring of the second string.
+ */
+
+public class PermutationInString {
+
+    public boolean checkInclusion(String s1, String s2) {
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1MatchesS2(s1, s2.substring(i, i + s1.length()))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean s1MatchesS2(String s1, String s2) {
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            ++arr[s1.charAt(i) - 'a'];
+            --arr[s2.charAt(i) - 'a'];
+        }
+
+        for (int a : arr) {
+            if (a != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
