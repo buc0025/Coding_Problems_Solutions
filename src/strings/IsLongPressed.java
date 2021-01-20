11be0d2b566e60bf545b5639cd66735542986712
buc0025 <stanley.yang001@gmail.com>
2021-01-19 21:38:15 -0500

    Random mock interview question from leetcode 01/19/2021

---
+++ src/strings/IsLongPressed.java
@@ -0,0 +1,45 @@
+package strings;
+/*
+https://leetcode.com/problems/long-pressed-name/
+Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the
+character will be typed 1 or more times.
+
+You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some
+characters (possibly none) being long pressed.
+ */
+
+public class IsLongPressed {
+
+    public boolean isLongPressedName(String name, String typed) {
+        if (name.length() > typed.length()) {
+            return false;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while (i < name.length()) {
+            if (j >= typed.length()) {
+                return false;
+            }
+            if (name.charAt(i) == typed.charAt(j)) {
+                i++;
+            } else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) {
+                return false;
+            }
+            j++;
+        }
+
+        char c = name.charAt(i - 1);
+
+        // Once the end of name is reached and letters are still remaining in typed, the rest of the letters should be the same
+        // as the last character of name
+        for (int k = j; k < typed.length(); k++) {
+            if (typed.charAt(k) != c) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
