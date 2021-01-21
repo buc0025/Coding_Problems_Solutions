e1314d26f9d65db45b3b6a0b3a0469e237de65ca
buc0025 <stanley.yang001@gmail.com>
2021-01-20 23:22:24 -0500

    Attempt at longest palindrome problem 01/20/2021

---
+++ src/strings/LongestPalindrome.java
@@ -0,0 +1,47 @@
+package strings;
+/*
+https://leetcode.com/problems/longest-palindrome/
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built
+with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+ */
+
+public class LongestPalindrome {
+
+    // Incorrect attempt
+    public int longestPalindrome(String s) {
+        int[] lower = new int[26];
+        int[] upper = new int[26];
+        int ans = 0;
+        int maxLow = 0;
+        int maxUp = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                ++lower[s.charAt(i) - 'a'];
+            } else {
+                ++upper[s.charAt(i) - 'A'];
+            }
+        }
+
+        for (int l : lower) {
+            if (l % 2 == 0) {
+                ans += l;
+            } else {
+                maxLow = Math.max(l, maxLow);
+            }
+        }
+
+        for (int u : upper) {
+            if (u % 2 == 0) {
+                ans += u;
+            } else {
+                maxUp = Math.max(u, maxUp);
+            }
+        }
+
+        return ans + Math.max(maxLow, maxUp);
+    }
+}
