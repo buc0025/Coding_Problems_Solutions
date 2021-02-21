d77ff4a6680ceda5433f3e6009fc9d9bf818cc56
buc0025 <stanley.yang001@gmail.com>
2021-02-20 22:16:35 -0500

    Attempt at previous troublesome leetcode problem 02/20/2021

--- src/arrays/RevealCardsInOrder.java
+++ src/arrays/RevealCardsInOrder.java
@@ -17,6 +17,29 @@ The first entry in the answer is considered to be the top of the deck.
 
 public class RevealCardsInOrder {
 
+    // Had to use hints to come up with solution
+    public int[] deckRevealedIncreasing(int[] deck) {
+        //Queue is used to hold the indices for new array
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < deck.length; i++) {
+            queue.add(i);
+        }
+
+        Arrays.sort(deck); //Sort deck is ascending order
+        int[] arr = new int[deck.length];
+
+        //Every other index is passed to the back of the queue and used later
+        for (int card : deck) {
+            arr[queue.poll()] = card;
+            if (!queue.isEmpty()) {
+                queue.add(queue.poll());
+            }
+        }
+
+        return arr;
+    }
+
     // Couldn't come up with solution on my own and had to study and write out given solutions
     public int[] deckRevealedIncreasing(int[] deck) {
         int[] arr = new int[deck.length];
