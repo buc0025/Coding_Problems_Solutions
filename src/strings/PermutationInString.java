caae08e162330a1208184bc32d67d50d71de1717
buc0025 <stanley.yang001@gmail.com>
2021-03-05 17:24:38 -0500

    Sliding window technique to solve permutation 03/05/2021

--- src/strings/PermutationInString.java
+++ src/strings/PermutationInString.java
@@ -7,6 +7,36 @@ first string's permutations is the substring of the second string.
 
 public class PermutationInString {
 
+    // Approach was to compare s1 to all substrings of s2 that's the length of s1
+    public boolean checkInclusion(String s1, String s2) {
+        int sLen = s1.length();
+
+        for (int i = 0; i <= s2.length() - sLen; i++) {
+            if (matchPerm(s1, s2.substring(i, i + sLen))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean matchPerm(String s1, String s2) {
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            arr[s1.charAt(i) - 'a']++;
+            arr[s2.charAt(i) - 'a']--;
+        }
+
+        for (int a : arr) {
+            if (a > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public boolean checkInclusion(String s1, String s2) {
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
             if (s1MatchesS2(s1, s2.substring(i, i + s1.length()))) {
