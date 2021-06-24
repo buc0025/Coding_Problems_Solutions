7896d19d94a623b7f70be69c3262baf1739b5885
buc0025 <stanley.yang001@gmail.com>
2021-06-24 08:36:33 -0400

    Coding assessment question from BenefitFocus

---
+++ src/arrays/MajorityElement.java
@@ -0,0 +1,42 @@
+package arrays;
+/*
+Question for coding assessment on 06/24/2021
+Majority element of an array is defined as a value x for which strictly more than half of array elements are equal to x. Output
+the majority element of the given array, or -1 if the array doesn't have majority element.
+ */
+
+public class MajorityElement {
+
+    public int majorityElement(String lengthOfArray, String numsInArray) {
+        String[] arr1 = numsInArray.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        int len = Integer.valueOf(lengthOfArray);
+        int count = 0;
+        int ans = -1;
+        int half = 0;
+
+        if (len % 2 == 0) {
+            half = half / 2;
+        } else {
+            half = len / 2 + 1;
+        }
+
+        for (String a : arr1) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+
+        for (String val : map.keySet()) {
+            if (map.get(val) >= count) {
+                count = Math.max(map.get(val), count);
+                ans = Integer.valueOf(val);
+            }
+        }
+
+        if (count >= half) {
+            System.out.println(ans);
+        } else {
+            System.out.println(-1);
+        }
+
+    }
+}
