7cf6c0d8d135a8664f775f726e5f47e0893cbc0c
buc0025 <stanley.yang001@gmail.com>
2021-07-03 21:34:01 -0400

    Given 2 strings, return true if s is a subsequence of t

---
+++ src/strings/IsSubsequence.java
@@ -0,0 +1,43 @@
+package strings;
+/*
+https://leetcode.com/problems/is-subsequence/
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters
+without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ */
+
+public class IsSubsequence {
+
+    /*
+    We only have to care about the order in string s. Once we find the first element in string t that matches with first char in
+    string s, we keep track of where the index was last left off in string t. We keep a count every time the elements match up and
+     compare it to the length of string s.
+     */
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) {
+            return true;
+        }
+
+        if (s.length() > t.length()) {
+            return false;
+        }
+
+        int index = 0;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            while (index < t.length()) {
+                if (s.charAt(i) == t.charAt(index)) {
+                    index++;
+                    count++;
+                    break;
+                } else {
+                    index++;
+                }
+            }
+        }
+
+        return count == s.length();
+    }
+}
