2e0b9e0a7ec382535448a2c5f35ccae4a20f9e21
buc0025 <stanley.yang001@gmail.com>
2021-07-17 17:58:29 -0400

    A two binary strings and return sum as binary using addition method with carry

--- src/strings/AddBinary.java
+++ src/strings/AddBinary.java
@@ -6,6 +6,68 @@ Given two binary strings a and b, return their sum as a binary string.
 
 public class AddBinary {
 
+    public String addBinary(String a, String b) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int aLen = a.length() - 1;
+        int bLen = b.length() - 1;
+        int carry = 0;
+
+        while (aLen >= 0 && bLen >= 0) {
+            int sum = (a.charAt(aLen) - '0') + (b.charAt(bLen) - '0') + carry;
+            if (sum == 3) {
+                carry = 1;
+                stringBuilder.append('1');
+            } else if (sum == 2) {
+                carry = 1;
+                stringBuilder.append('0');
+            } else if (sum == 1) {
+                carry = 0;
+                stringBuilder.append('1');
+            } else {
+                carry = 0;
+                stringBuilder.append('0');
+            }
+            aLen--;
+            bLen--;
+        }
+
+        while (aLen >= 0) {
+            int sum = (a.charAt(aLen) - '0') + carry;
+            if (sum == 2) {
+                carry = 1;
+                stringBuilder.append('0');
+            } else if (sum == 1) {
+                carry = 0;
+                stringBuilder.append('1');
+            } else {
+                carry = 0;
+                stringBuilder.append('0');
+            }
+            aLen--;
+        }
+
+        while (bLen >= 0) {
+            int sum = (b.charAt(bLen) - '0') + carry;
+            if (sum == 2) {
+                carry = 1;
+                stringBuilder.append('0');
+            } else if (sum == 1) {
+                carry = 0;
+                stringBuilder.append('1');
+            } else {
+                carry = 0;
+                stringBuilder.append('0');
+            }
+            bLen--;
+        }
+
+        if (carry == 1) {
+            stringBuilder.append('1');
+        }
+
+        return stringBuilder.reverse().toString();
+    }
+
     /* Solution passes test cases for binary numbers up to max value of long. Need to come up with different solution that only
     involves adding binary without the decimal value.
      */
