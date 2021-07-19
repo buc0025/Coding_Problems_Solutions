9b121d881bc8bf2b9928d0f4a3c9d494db82b2e2
buc0025 <stanley.yang001@gmail.com>
2021-07-19 18:01:31 -0400

    Determine if collection of tiles can be rearranged to form string

---
+++ src/strings/TileArranger.java
@@ -0,0 +1,48 @@
+package strings;
+/*
+Coding assessment problem from Whole Foods.
+Write an algorithm to determine whether a collection of tiles can be rearranged to form a given word. Each tile has 1..N letters.
+You do not have to use each tile, but you cannot use any tile more than once. There can be several identical tiles.
+
+String word = "foobarbaz"
+String[] arr = {"foob", "foo", "ba", "ba", "r", "z"};
+Answer = true
+ */
+
+public class TileArranger {
+
+    public boolean tileArranger(String s, String[] arr) {
+        Set<Integer> set = new HashSet<>(); // makes sure tile is only used once
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            /* If the substring of s is equal to the word in the array then enter second loop to search for next matching word in
+            substring
+            */
+            if (s.substring(index, index + word.length()).equals(word)) {
+                set.add(i);
+                index += word.length();
+                for (int j = 0; j < arr.length; j++) {
+                    String word2 = arr[j];
+                    /* If the substring of s is equal to the word in the array then start next iteration at j to search for next
+                    matching word in substring
+                    */
+                    if (!set.contains(j) && s.substring(index, index + word2.length()).equals(word2)) {
+                        set.add(j);
+                        index += word2.length();
+                        i = j;
+                        break;
+                    }
+                }
+            }
+            if (index == s.length() - 1) {
+                System.out.println(true);
+                break;
+            }
+            // start over from the beginning of the given string for comparison and clear the set.
+            index = 0;
+            set.clear();
+        }
+    }
+}
