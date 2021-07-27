af8b43ac05c97473b9a5cac55b3562fe56a8518b
buc0025 <stanley.yang001@gmail.com>
2021-07-26 21:40:29 -0400

    Finding number of equivalent domino pairs in o(n) time

--- src/arrays/EquivalentDominoPairs.java
+++ src/arrays/EquivalentDominoPairs.java
@@ -9,6 +9,38 @@ Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and do
 
 public class EquivalentDominoPairs {
 
+    /* Solution counts number of equivalent domino pairs but misunderstanding given solution to
+     [[2,1],[1,2],[1,2],[1,2],[2,1],[1,1],[1,2],[2,2]] which should be 6 but expected output says 15
+     */
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int count = 0;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int[] d : dominoes) {
+            String left = d[0] + "" + d[1];
+            String right = d[1] + "" + d[0];
+
+            if (map.containsKey(left)) {
+                map.put(left, map.getOrDefault(left, 0) + 1);
+            } else if (map.containsKey(right)) {
+                map.put(right, map.getOrDefault(right, 0) + 1);
+            } else {
+                map.put(left, 0);
+            }
+        }
+
+        for (String n : map.keySet()) {
+            if (map.get(n) > 1) {
+                count += map.get(n) + 1;
+            } else {
+                count += map.get(n);
+            }
+        }
+
+        return count;
+    }
+
     // Brute force solution comparing current element array with every following element
     public int numEquivDominoPairs(int[][] dominoes) {
         int count = 0;
