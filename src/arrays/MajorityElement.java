eb1539fdc1a34577f58fc322239eb16115b84eff
buc0025 <stanley.yang001@gmail.com>
2021-08-16 21:37:48 -0400

    Find majority element in array which is more than half of array length

--- src/arrays/MajorityElement.java
+++ src/arrays/MajorityElement.java
@@ -7,6 +7,37 @@ the majority element of the given array, or -1 if the array doesn't have majorit
 
 public class MajorityElement {
 
+    //Different solution without having to use a map
+    public int majorityElement(String lengthOfArray, String numsInArray) {
+            int len = Integer.parseInt(lengthOfArray);
+            int half = 0;
+            int result = -1;
+
+            if (len % 2 == 0) {
+                half = len / 2;
+            } else {
+                half = len / 2 + 1;
+            }
+
+            int[] nums = new int[101];
+
+            for (int i = 0; i < numsInArray.length(); i++) {
+                if (numsInArray.charAt(i) != ' ') {
+                    int val = Integer.parseInt(numsInArray.substring(i, i + 1));
+                    nums[val]++;
+                }
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= half) {
+                    result = i;
+                    break;
+                }
+            }
+
+            System.out.println(result);
+    }
+
     public int majorityElement(String lengthOfArray, String numsInArray) {
         String[] arr1 = numsInArray.split(" ");
         Map<String, Integer> map = new HashMap<>();
