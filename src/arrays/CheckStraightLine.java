fcda05f69a27285b7b4437ef8e5598437bae119e
buc0025 <stanley.yang001@gmail.com>
2021-08-21 16:09:42 -0400

    Check if coordinates in matrix form a straight line on XY plane

---
+++ src/arrays/CheckStraightLine.java
@@ -0,0 +1,53 @@
+package arrays;
+/*
+https://leetcode.com/problems/check-if-it-is-a-straight-line/
+You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these
+points make a straight line in the XY plane.
+ */
+
+public class CheckStraightLine {
+
+    // Passes 75/79 test cases
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x1 = coordinates[0][0];
+        int y1 = coordinates[0][1];
+        int x2 = coordinates[1][0];
+        int y2 = coordinates[1][1];
+        int slope = 0;
+
+        if (x2 - x1 != 0) {
+            slope = (y2 - y1) / (x2 - x1);
+        }
+
+        for (int i = 2; i < coordinates.length; i++) {
+            int x1a = coordinates[i - 1][0];
+            int y1a = coordinates[i - 1][1];
+            int x2a = coordinates[i][0];
+            int y2a = coordinates[i][1];
+
+            if (x2a - x1a == 0) {
+                return slope == 0;
+            }
+
+            if (slope != (y2a - y1a) / (x2a - x1a)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Incorrect solution using 0(n2) on 7/15/2020
+    public boolean checkStraightLine(int[][] coordinates) {
+        int n = coordinates[1][0] - coordinates[1][1];
+
+        for (int i = 1; i < coordinates.length; i++) {
+            for (int j = 0; j < coordinates[i].length-1; j++) {
+                if (n != coordinates[i][j] - coordinates[i][j+1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
