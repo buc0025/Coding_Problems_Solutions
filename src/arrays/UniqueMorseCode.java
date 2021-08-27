f64cb3b8d179a76c8592485862d83b716b0b20b8
buc0025 <stanley.yang001@gmail.com>
2021-08-26 22:10:05 -0400

    Return number of different transformations of morse code in string arr

---
+++ src/arrays/UniqueMorseCode.java
@@ -0,0 +1,42 @@
+package arrays;
+/*
+https://leetcode.com/problems/unique-morse-code-words/
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-",
+"...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a
+concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+ */
+
+public class UniqueMorseCode {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] arr = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",
+                ".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> set = new HashSet<>();
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String word : words) {
+
+            for (int i = 0; i < word.length(); i++) {
+                int val = word.charAt(i) - 97;
+                stringBuilder.append(arr[val]);
+            }
+
+            set.add(stringBuilder.toString());
+            stringBuilder.setLength(0);
+        }
+
+        return set.size();
+    }
+}
