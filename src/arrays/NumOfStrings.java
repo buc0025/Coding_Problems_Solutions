5b063aedf8e44595dddcca3f86f43a8490e48a62
buc0025 <stanley.yang001@gmail.com>
2021-08-31 23:08:25 -0400

    Optimal solution to Leetcode problem #1967

--- src/arrays/NumOfStrings.java
+++ src/arrays/NumOfStrings.java
@@ -12,6 +12,18 @@ public class NumOfStrings {
         int result = 0;
 
         for (String pattern : patterns) {
+            if (word.indexOf(pattern) != -1) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public int numOfStrings(String[] patterns, String word) {
+        int result = 0;
+
+        for (String pattern : patterns) {
             int len = pattern.length();
 
             if (len > word.length()) {
