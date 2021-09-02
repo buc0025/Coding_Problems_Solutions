92daa5fff3510b525795059a53cfb04ca77d6b05
buc0025 <stanley.yang001@gmail.com>
2021-09-01 22:36:40 -0400

    Using Math.sqrt(n) to optimize time complexity to find prime numbers

--- src/arrays/CountPrimes.java
+++ src/arrays/CountPrimes.java
@@ -9,13 +9,16 @@ import java.util.Set;
 
 public class CountPrimes {
 
-    // Exceeds time limit but guarantees only prime numbers are counted.
+    /* Exceeds time limit but guarantees only prime numbers are counted.
+    Finding out if a number is prime is to check whether it is evenly divisible by any prime number between 2 and square root
+    of the number
+    */
     public int countPrimes(int n) {
         int result = 0;
 
         for (int i = 2; i < n; i++) {
             boolean prime = true;
-            for (int j = 2; j < i; j++) {
+            for (int j = 2; j < Math.sqrt(i); j++) {
                 if (i % j == 0) {
                     prime = false;
                     break;
