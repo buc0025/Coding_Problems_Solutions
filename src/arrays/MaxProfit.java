e501abb1144d3abf1eb94a79217672b66aa808a4
buc0025 <stanley.yang001@gmail.com>
2021-09-07 22:49:29 -0400

    Return max profit of best time to buy and sell stock

---
+++ src/arrays/MaxProfit.java
@@ -0,0 +1,28 @@
+package arrays;
+/*
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that
+stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ */
+
+public class MaxProfit {
+
+    // Time limit exceeded. Need to find better solution than o(n2) 
+    public int maxProfit(int[] prices) {
+        int max = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] > prices[i]) {
+                    max = Math.max(max, prices[j] - prices[i]);
+                }
+            }
+        }
+
+        return max;
+    }
+}
