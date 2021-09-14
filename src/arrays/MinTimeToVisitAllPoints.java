e7fad4aa75a51c09a7acbd2772dbd7532cb3ee27
buc0025 <stanley.yang001@gmail.com>
2021-09-13 21:54:40 -0400

    Explanation for leetcode #1266 Minimum time visiting all pts

--- src/arrays/MinTimeToVisitAllPoints.java
+++ src/arrays/MinTimeToVisitAllPoints.java
@@ -16,20 +16,28 @@ You are allowed to pass through points that appear later in the order, but these
 
 public class MinTimeToVisitAllPoints {
 
+    /* It doesn't matter if the next point is to the left or right of the current as long as we know how far away it is
+    horizontally. The same goes for up and down. Moving diagonally would be the shortest route if the points are not on the same
+    axis. To get the time for diagonal movements, we take the max of horizontal and vertical moves. This is still less than adding
+    both moves together.
+     */
+
     public int minTimeToVisitAllPoints(int[][] points) {
+        // Starting points which will change with each iteration
         int startX = points[0][0];
         int startY = points[0][1];
 
         int time = 0;
 
         for (int i = 1; i < points.length; i++) {
-            int vertical = Math.abs(startX - points[i][0]);
-            int horizontal = Math.abs(startY - points[i][1]);
+            int vertical = Math.abs(startX - points[i][0]); // Same number of steps whether left or right
+            int horizontal = Math.abs(startY - points[i][1]); // Same number of steps whether up or down
 
+            // new points to compare to next points in order
             startX = points[i][0];
             startY = points[i][1];
 
-            time += Math.max(vertical, horizontal);
+            time += Math.max(vertical, horizontal); // adding diagonal/vertical or horizontal moves
         }
 
         return time;
