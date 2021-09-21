efc94a345d60b0a0740482535495bf80e9013b94
buc0025 <stanley.yang001@gmail.com>
2021-09-21 16:17:23 -0400

    Return final value of variable after performing operations

---
+++ src/arrays/FinalValueAfterOperations.java
@@ -0,0 +1,29 @@
+package arrays;
+/*
+https://leetcode.com/problems/final-value-of-variable-after-performing-operations/submissions/
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the
+operations.
+ */
+
+public class FinalValueAfterOperations {
+
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+
+        for (String operation : operations) {
+            if (operation.contains("++")) {
+                result++;
+            } else {
+                result--;
+            }
+        }
+
+        return result;
+    }
+}
