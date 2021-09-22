29d50bcc05ffaa1ca234296039ba54cd1b998e60
buc0025 <stanley.yang001@gmail.com>
2021-09-22 12:21:34 -0400

    Count number of pairs with absolute difference k

---
+++ src/arrays/CountKDifference.java
@@ -0,0 +1,23 @@
+package arrays;
+/*
+https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/
+
+Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+ */
+
+public class CountKDifference {
+
+    public int countKDifference(int[] nums, int k) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
