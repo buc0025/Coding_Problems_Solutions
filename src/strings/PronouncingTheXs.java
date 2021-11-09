5466914674dcebce3cc69aa76594cd7b699ed114
buc0025 <stanley.yang001@gmail.com>
2021-11-08 21:33:26 -0500

    Remaining piece to replace all x's in string, Edabit

--- src/strings/PronouncingTheXs.java
+++ src/strings/PronouncingTheXs.java
@@ -12,6 +12,35 @@ The word is just the letter "x", therefore replace it with "ecks".
 
 public class PronouncingTheXs {
 
+    // Top solution was to do a replaceAll for " x ", " x", and "x"
+    public static String xPronounce(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String word : words) {
+            int len = word.length();
+            if (word.equals("x")) {
+                stringBuilder.append("ecks");
+            } else if (word.charAt(0) == 'x') {
+                stringBuilder.append("z");
+                stringBuilder.append(word.substring(1));
+            } else if (word.contains("x")) {
+                for (int i = 0; i < len; i++) {
+                    if (word.charAt(i) == 'x') {
+                        stringBuilder.append("cks");
+                    } else {
+                        stringBuilder.append(word.charAt(i));
+                    }
+                }
+            } else {
+                stringBuilder.append(word);
+            }
+            stringBuilder.append(" ");
+        }
+
+        return stringBuilder.substring(0, stringBuilder.length() - 1).toString();
+    }
+
     // Does not pass all test cases. Solution only accounts for words beginning with x and words with just x.
     public static String xPronounce(String sentence) {
         String[] words = sentence.split(" ");
