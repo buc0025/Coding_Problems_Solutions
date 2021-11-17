fc547266289f3ac4a55de6a9ecf6f35e571c1a46
buc0025 <stanley.yang001@gmail.com>
2021-11-16 20:29:58 -0500

    Edabit's Positive Dominant problem without using sets

--- src/arrays/PositiveDominant.java
+++ src/arrays/PositiveDominant.java
@@ -8,6 +8,33 @@ that returns true if an array is positive dominant, false, otherwise.
 
 public class PositiveDominant {
 
+    //Incorrect solution. Passes 17/18 test cases
+    public static boolean isPositiveDominant(int[] n) {
+        Arrays.sort(n);
+        int positive = 0;
+        int negative = 0;
+
+        if (n[0] != n[1]) {
+            if (n[0] > 0) {
+                positive++;
+            } else if (n[0] < 0) {
+                negative++;
+            }
+        }
+
+        for (int i = 1; i < n.length; i++) {
+            if (n[i] != n[i - 1]) {
+                if (n[i] > 0) {
+                    positive++;
+                } else if (n[i] < 0) {
+                    negative++;
+                }
+            }
+        }
+
+        return positive > negative;
+    }
+
     public static boolean isPositiveDominant(int[] n) {
         Set<Integer> positive = new HashSet<>();
         Set<Integer> negative = new HashSet<>();
