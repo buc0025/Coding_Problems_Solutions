fad75c8d85756461a5f0b2299888b2b6f868bf73
buc0025 <stanley.yang001@gmail.com>
2021-11-21 20:25:40 -0500

    Return highest scoring word according to its position in alphabet

---
+++ src/strings/WordRank.java
@@ -0,0 +1,38 @@
+package strings;
+/*
+https://edabit.com/challenge/63GZYcgtT7XiSjsKL
+
+Create a function that takes a string of words and returns the highest scoring word. Each letter of a word scores points according
+to it's position in the alphabet: a = 1, b = 2, c = 3, etc.
+ */
+
+public class WordRank {
+
+    // Incorrect solution. Also returns punctuation.
+    public static String word_rank(String str) {
+        String[] words = str.split(" ");
+        int[] nums = new int[words.length];
+        int max = 0;
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            int score = 0;
+            String word = words[i].toLowerCase();
+            for (int j = 0; j < word.length(); j++) {
+                if (Character.isLetter(word.charAt(j))) {
+                    score += word.charAt(j) - 'a';
+                }
+            }
+            nums[i] = score;
+            max = Math.max(score, max);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == max) {
+                return words[i];
+            }
+        }
+
+        return "not found";
+    }
+}
