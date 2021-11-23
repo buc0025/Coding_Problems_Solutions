23630fe821160dd2005c6c667de3da3e469ecacc
buc0025 <stanley.yang001@gmail.com>
2021-11-22 20:50:03 -0500

    Corrected solution to WordRank Scoring System problem, Edabit

--- src/strings/WordRank.java
+++ src/strings/WordRank.java
@@ -8,6 +8,37 @@ to it's position in the alphabet: a = 1, b = 2, c = 3, etc.
 
 public class WordRank {
 
+    public static String word_rank(String str) {
+        String[] words = str.split(" ");
+        int[] nums = new int[words.length];
+        int max = 0;
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            int score = 0;
+            String word = words[i].toLowerCase();
+            for (int j = 0; j < word.length(); j++) {
+                if (Character.isLetter(word.charAt(j))) {
+                    score += word.charAt(j) - 'a' + 1;
+                }
+            }
+            nums[i] = score;
+            max = Math.max(score, max);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == max) {
+                for (int j = 0; j < words[i].length(); j++) {
+                    if (Character.isLetter(words[i].charAt(j))) {
+                        result += words[i].charAt(j);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
     // Incorrect solution. Also returns punctuation.
     public static String word_rank(String str) {
         String[] words = str.split(" ");
