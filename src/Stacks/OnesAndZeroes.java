210fd589f346f10f47f50c1a76bbc86723cb7133
buc0025 <stanley.yang001@gmail.com>
2021-11-25 21:41:25 -0500

    Ones and Zeroes problem from Edabit

--- src/Stacks/OnesAndZeroes.java
+++ src/Stacks/OnesAndZeroes.java
@@ -7,6 +7,26 @@ Write a function that returns true if every consecutive sequence of ones is foll
 
 public class OnesAndZeroes {
 
+    public static boolean sameLength(String str) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                stack.push(1);
+            } else if (stack.empty()) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+
+        if (!stack.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+
     // Solution passes 9 out of 12 test cases
     public static boolean sameLength(String str) {
         Stack<Integer> stack = new Stack<>();
