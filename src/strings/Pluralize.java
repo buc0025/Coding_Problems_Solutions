f91fec5f38811088649d626776cca73d1b7833de
buc0025 <stanley.yang001@gmail.com>
2021-11-29 21:19:48 -0500

    Return set of plural form words if they appear more than once, Edabit

--- src/strings/Pluralize.java
+++ src/strings/Pluralize.java
@@ -8,25 +8,31 @@ list.
 
 public class Pluralize {
 
-    // Solution is not returning the strings in order
+    // Top result uses arraylist to keep track of plurals using contains() and replacing position of word with 's' at the end if
+    // it appears in array again
     public static String[] pluralize(String[] s) {
         Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
 
         for (String word : s) {
             map.put(word, map.getOrDefault(word, 0) + 1);
+            set.add(word);
         }
 
         String[] words = new String[map.size()];
         int index = 0;
 
-        for (String word : map.keySet()) {
-            if (map.get(word) > 1) {
-                String plural = word + "s";
-                words[index] = plural;
-            } else {
-                words[index] = word;
+        for (String word : s) {
+            if (set.contains(word)) {
+                if (map.get(word) > 1) {
+                    String plural = word + "s";
+                    words[index] = plural;
+                } else {
+                    words[index] = word;
+                }
+                index++;
             }
-            index++;
+            set.remove(word);
         }
 
         return words;
