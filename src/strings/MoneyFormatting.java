82511c239ed155468982e953c22aa25501203fd9
buc0025 <stanley.yang001@gmail.com>
2021-11-30 21:12:15 -0500

    Given a double, return string formatted in US dollars and cents

---
+++ src/strings/MoneyFormatting.java
@@ -0,0 +1,63 @@
+package strings;
+/*
+https://edabit.com/challenge/7r3uQADjtXEk7qirj
+
+Given a number, return a string which is formatted into US Dollars and cents!
+
+Remember that:
+
+You should round to two digits after the decimal point (even for integers).
+Thousandths should be separated by commas.
+ */
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class MoneyFormatting {
+
+    public static String dolladollaBills(double money) {
+        String cents = ".00";
+        boolean negative = false;
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        String s = df.format(money);
+
+        if (s.charAt(0) == '-') {
+            s = s.substring(1);
+            negative = true;
+        }
+
+        if (s.contains(".")) {
+            int decimal = s.indexOf(".");
+            int len = s.length();
+            if (len - decimal == 2) {
+                cents = s.substring(decimal) + "0";
+            } else {
+                cents = s.substring(decimal);
+            }
+            s = s.substring(0, decimal);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int comma = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (comma == 3) {
+                stringBuilder.append(",");
+                comma = 0;
+            }
+            stringBuilder.append(s.charAt(i));
+            comma++;
+        }
+
+        String result = "";
+
+        if (negative) {
+            result += "-$" + stringBuilder.reverse().toString() + cents;
+        } else {
+            result += "$" + stringBuilder.reverse().toString() + cents;
+        }
+
+        return result;
+    }
+}
