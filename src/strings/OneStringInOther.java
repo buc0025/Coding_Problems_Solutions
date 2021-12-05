ce1e1009e453c479625c8ea6dc5841e7c6e09882
buc0025 <stanley.yang001@gmail.com>
2021-12-04 21:18:19 -0500

    Determine if string appears at the end of other string, Edabit

--- src/strings/OneStringInOther.java
+++ src/strings/OneStringInOther.java
@@ -8,6 +8,33 @@ Return false otherwise. The character * is a wildcard, so it can take the place 
 
 public class OneStringInOther {
 
+    public static boolean overlap(String str1, String str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        if (len1 < len2) {
+            String end = str2.substring(len2 - len1);
+            for (int i = 0; i < len1; i++) {
+                char c = str1.charAt(i);
+                if (end.charAt(i) != '*' && end.charAt(i) != c && c != '*') {
+                    return false;
+                }
+            }
+        } else {
+            String end = str1.substring(len1 - len2);
+            for (int i = 0; i < len2; i++) {
+                char c = str2.charAt(i);
+                if (end.charAt(i) != '*' && end.charAt(i) != c && c != '*') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     // Solution does not take into account of strings appearing only at the very end of other string
     public static boolean overlap(String str1, String str2) {
         str1 = str1.replaceAll("\\*", "").toLowerCase();
