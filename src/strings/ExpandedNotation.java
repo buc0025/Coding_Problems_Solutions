1c6c916b789ae58562570a6f0bfcf123cdaceb6c
buc0025 <stanley.yang001@gmail.com>
2021-12-18 20:49:36 -0500

    Return a string of given number in expanded notation

---
+++ src/strings/ExpandedNotation.java
@@ -0,0 +1,32 @@
+package strings;
+/*
+https://edabit.com/challenge/f72omtBYFEBotgdEX
+
+Create a function that takes a number and return a string with the number in expanded notation (AKA expanded form). See the
+resources tab for details on expanded notation.
+ */
+
+public class ExpandedNotation {
+
+    // Solution doesn't deal with zeroes in given num
+    public static String expand(long num) {
+        String number = String.valueOf(num);
+        int zeroes = number.length() - 1;
+        StringBuilder expanded = new StringBuilder();
+
+        for (int i = 0; i < number.length(); i++) {
+            expanded.append(number.charAt(i));
+            for (int j = 0; j < zeroes; j++) {
+                expanded.append(0);
+            }
+
+            if (i != number.length() - 1) {
+                expanded.append(" + ");
+            }
+
+            zeroes--;
+        }
+
+        return expanded.toString();
+    }
+}
