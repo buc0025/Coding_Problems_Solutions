3b993a3e2aa19ce16b103d6912824d3d1b3748c7
buc0025 <stanley.yang001@gmail.com>
2021-12-19 20:26:56 -0500

    Return a string of given number in expanded notation

--- src/strings/ExpandedNotation.java
+++ src/strings/ExpandedNotation.java
@@ -8,6 +8,33 @@ resources tab for details on expanded notation.
 
 public class ExpandedNotation {
 
+    public static String expand(long num) {
+        String number = String.valueOf(num);
+        int zeroes = number.length() - 1;
+        String expand = "";
+
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) == '0') {
+                zeroes--;
+                continue;
+            }
+            expand += number.substring(i, i + 1);
+            for (int j = 0; j < zeroes; j++) {
+                expand += "0";
+            }
+            if (i != number.length() - 1) {
+                expand += " + ";
+            }
+            zeroes--;
+        }
+
+        if (expand.substring(expand.length() - 3).equals(" + ")) {
+            return expand.substring(0, expand.length() - 3);
+        }
+
+        return expand;
+    }
+
     // Solution doesn't deal with zeroes in given num
     public static String expand(long num) {
         String number = String.valueOf(num);
