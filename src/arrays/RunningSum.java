8fa0640b58b7c13089319c7858ce463f99df3148
buc0025 <stanley.yang001@gmail.com>
2022-03-22 20:57:35 -0400

    Define a running sum of an array. Leetcode 1480

---
+++ src/arrays/RunningSum.java
@@ -0,0 +1,22 @@
+package arrays;
+/*
+https://leetcode.com/problems/running-sum-of-1d-array/
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+ */
+
+public class RunningSum {
+
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+
+        return nums;
+    }
+}
