9368fc90015ebde911021bba4a6bb8993de65b70
buc0025 <stanley.yang001@gmail.com>
2022-03-26 21:01:38 -0400

    Find max number of words in array with sentences. Leetcode 2114

---
+++ src/arrays/MostWordsFound.java
@@ -0,0 +1,30 @@
+package arrays;
+/*
+https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+Return the maximum number of words that appear in a single sentence.
+ */
+
+public class MostWordsFound {
+
+    //  3/26/2022 solution
+    public int mostWordsFound(String[] sentences) {
+        int max = 1;
+
+        for (int i = 0; i < sentences.length; i++) {
+            int space = 1;
+            for (int j = 0; j < sentences[i].length(); j++) {
+                if (sentences[i].charAt(j) == ' ') {
+                    space++;
+                }
+            }
+            max = Math.max(max, space);
+        }
+
+        return max;
+    }
+}
