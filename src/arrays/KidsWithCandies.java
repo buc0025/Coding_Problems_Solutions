6f3a5859dfd085247b8bbbf98d59e4589848a762
buc0025 <stanley.yang001@gmail.com>
2022-04-01 21:19:53 -0400

    Return boolean array with kids with mmost candy. Leetcode 1431

---
+++ src/arrays/KidsWithCandies.java
@@ -0,0 +1,37 @@
+package arrays;
+/*
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the
+ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will
+have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+ */
+
+public class KidsWithCandies {
+
+    // 4/1/2022 solution
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> maxCandy = new ArrayList<>();
+        int max = 0;
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                maxCandy.add(true);
+            } else {
+                maxCandy.add(false);
+            }
+        }
+
+        return maxCandy;
+    }
+}
