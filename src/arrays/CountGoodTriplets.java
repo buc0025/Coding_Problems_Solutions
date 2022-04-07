da824eaeb65ce314add36b1ac2a8d6eebc91eb6b
buc0025 <stanley.yang001@gmail.com>
2022-04-06 20:54:32 -0400

    Find number of good triplets according to specs in array. Leetcode 1534

---
+++ src/arrays/CountGoodTriplets.java
@@ -0,0 +1,36 @@
+package arrays;
+/*
+https://leetcode.com/problems/count-good-triplets/
+
+Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+
+A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+0 <= i < j < k < arr.length
+|arr[i] - arr[j]| <= a
+|arr[j] - arr[k]| <= b
+|arr[i] - arr[k]| <= c
+Where |x| denotes the absolute value of x.
+
+Return the number of good triplets.
+ */
+
+public class CountGoodTriplets {
+
+    // 4/6/2022 solution
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int result = 0;
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                        result++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
