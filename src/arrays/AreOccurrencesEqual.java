41e972c640dfdf1eddbb53f60b895c49b0dd51d0
buc0025 <stanley.yang001@gmail.com>
2022-04-15 20:46:51 -0400

    Check if all characters have equal number of occurrences. Leetcode 1941

--- src/arrays/AreOccurrencesEqual.java
+++ src/arrays/AreOccurrencesEqual.java
@@ -8,6 +8,52 @@ A string s is good if all the characters that appear in s have the same number o
 
 public class AreOccurrencesEqual {
 
+    // 4/15/2022 solution
+    public boolean areOccurrencesEqual(String s) {
+        int[] arr = new int[26];
+        int occurrences = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (occurrences != 0) {
+                if (arr[i] != 0 && arr[i] != occurrences) {
+                    return false;
+                }
+            }
+            if (arr[i] != 0) {
+                occurrences = arr[i];
+            }
+        }
+
+        return true;
+    }
+
+    // 4/15/2022 solution
+    public boolean areOccurrencesEqual(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        int occurrences = map.get(s.charAt(0));
+
+        for (Character c : map.keySet()) {
+            if (map.get(c) != occurrences) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public boolean areOccurrencesEqual(String s) {
         int[] alphabet = new int[26];
         int count = 0;
