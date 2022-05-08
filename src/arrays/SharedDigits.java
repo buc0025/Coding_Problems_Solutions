09c47e651636462cbc39866806d8b3774de706f1
buc0025 <stanley.yang001@gmail.com>
2022-05-07 20:38:42 -0400

    Return true if each pair of adjacent nums share at least 1 digit

---
+++ src/arrays/SharedDigits.java
@@ -0,0 +1,33 @@
+package arrays;
+/*
+https://edabit.com/challenge/rCkeAiRCpduosZ4Hj
+
+Create a function that returns true if each pair of adjacent numbers in an array shares at least one digit and false otherwise.
+ */
+
+public class SharedDigits {
+
+    // 5/7/2022 unfinished solution
+    public static boolean sharedDigits(int[] num) {
+        boolean shared = false;
+        for (int i = 0; i < num.length - 1; i++) {
+            Set<Character> set = new HashSet<>();
+            String number1 = String.valueOf(num[i]);
+            String number2 = String.valueOf(num[i + 1]);
+            for (int j = 0; j < number1.length(); j++) {
+                set.add(number1.charAt(j));
+            }
+            for (int k = 0; k < number2.length(); k++) {
+                if (set.contains(number2.charAt(k))) {
+                    shared = true;
+                    break;
+                }
+            }
+            if (!shared) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
