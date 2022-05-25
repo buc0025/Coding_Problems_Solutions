914839827ff05244473596aef40d8ebf5dad7f59
buc0025 <stanley.yang001@gmail.com>
2022-05-25 18:57:13 -0400

    Leetcode 345 Reverse Vowels of a String

--- src/strings/ReverseVowels.java
+++ src/strings/ReverseVowels.java
@@ -6,6 +6,41 @@ Write a function that takes a string as input and reverse only the vowels of a s
 
 public class ReverseVowels {
 
+    // 5/25/2022 solution
+    public String reverseVowels(String s) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        StringBuilder word = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                word.append(stack.pop());
+            } else {
+                word.append(s.charAt(i));
+            }
+        }
+
+        return word.toString();
+    }
+
     public String reverseVowels(String s) {
         StringBuilder vowels = new StringBuilder();
         StringBuilder result = new StringBuilder();
