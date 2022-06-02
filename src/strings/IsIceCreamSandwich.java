da0b532d073e17865f2ee7b1cf2348dbae8d8355
buc0025 <stanley.yang001@gmail.com>
2022-06-01 20:56:46 -0400

    Edabit's Ice Cream Sandwich problem

--- src/strings/IsIceCreamSandwich.java
+++ src/strings/IsIceCreamSandwich.java
@@ -7,25 +7,35 @@ An ice cream sandwich is a string that is formed by two identical ends and a dif
 
 public class IsIceCreamSandwich {
 
-    // 5/31/2022 unfinished solution
+    // 6/1/2022 solution
     public static boolean isIcecreamSandwich(String str) {
         if (str.length() < 3) {
-            System.out.println(false);
+            return false;
         }
 
-        String front = str.substring(0,1);
-        String middle = "";
-        String end = str.substring(str.length() - 1);
+        int frontEnd = 0;
+        int middleStart = 0;
+        int middleEnd = 0;
 
-        System.out.println(front);
-        System.out.println(middle);
-
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (str.charAt(i) != str.charAt(i + 1)) {
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i - 1)) {
+                frontEnd = i - 1;
+                middleStart = frontEnd + 1;
                 break;
-            } else {
-                front += str.substring(i, i + 1);
             }
         }
+
+        for (int i = middleStart + 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i - 1)) {
+                middleEnd = i - 1;
+                break;
+            }
+        }
+
+        if (!str.substring(0, frontEnd + 1).equals(str.substring(middleEnd + 1))) {
+            return false;
+        }
+
+        return true;
     }
 }
