8b468b7b60b620b54d4fa064e2be1dbc2929bbff
buc0025 <stanley.yang001@gmail.com>
2022-06-09 20:48:19 -0400

    Unfinished solution to Edabit's KixCodes problem

--- src/strings/KixCode.java
+++ src/strings/KixCode.java
@@ -12,15 +12,34 @@ If there is a character between the house number and the suffix, we need to repl
 
 public class KixCode {
 
-    // 6/8/2022 unfinished solution
+    // 6/9/2022 unfinished solution
     public static String kixCode(String addr) {
         String[] arr = addr.split(", ");
         String house = arr[2].substring(0, 4);
         String box = arr[2].substring(5,7);
 
-        int len = arr[1].length();
-        String callNum = arr[1].substring(len - 5);
+        int start = 0;
 
-        return house + box + callNum;
+        for (int i = 0; i < arr[1].length(); i++) {
+            if (Character.isDigit(arr[1].charAt(i))) {
+                start = i;
+                break;
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = start; i < arr[1].length(); i++) {
+            char n = arr[1].charAt(i);
+            if (Character.isDigit(n)) {
+                stringBuilder.append(n);
+            } else if (n == ' ') {
+                stringBuilder.append('X');
+            } else {
+                stringBuilder.append(arr[1].substring(i, i + 1).toUpperCase());
+            }
+        }
+
+        return house + box + stringBuilder.toString();
     }
 }
