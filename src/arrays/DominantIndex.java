90cb0cc1f078a0679582af01a23eb79dc8004d9a
buc0025 <stanley.yang001@gmail.com>
2022-07-26 21:05:32 -0400

    Leetcode 747 Largest Number At Least Twice of Others

--- src/arrays/DominantIndex.java
+++ src/arrays/DominantIndex.java
@@ -9,6 +9,28 @@ the index of the largest element, or return -1 otherwise.
 
 public class DominantIndex {
 
+    // 7/26/2022  Top solution was to solve with 1 loop and keep track of the second biggest number and check if max is at least
+    // twice its size
+    public int dominantIndex(int[] nums) {
+        int max = 0;
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != index && max < nums[i] * 2) {
+                return -1;
+            }
+        }
+
+        return index;
+    }
+
     public int dominantIndex(int[] nums) {
         int max = 0;
         int index = 0;
