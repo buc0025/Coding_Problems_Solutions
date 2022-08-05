5b4d41d424c9135fada7e3da2e8ea00b8901d66e
buc0025 <stanley.yang001@gmail.com>
2022-08-04 20:43:30 -0400

    Unfinished solution to leetcode 819

--- src/Maps_Sets_Tables/MostCommonWord.java
+++ src/Maps_Sets_Tables/MostCommonWord.java
@@ -9,6 +9,45 @@ The words in paragraph are case-insensitive and the answer should be returned in
 
 public class MostCommonWord {
 
+    // 8/4/2022 unfinished solution
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.toLowerCase();
+        paragraph = paragraph.replaceAll("[^a-zA-Z0-9]", " ");
+
+        String[] words = paragraph.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (word.length() > 0) {
+                if (map.containsKey(word)) {
+                    map.put(word, map.get(word) + 1);
+                } else {
+                    map.put(word, 1);
+                }
+            }
+        }
+
+        int max = 0;
+        String answer = "";
+
+        for (String key : map.keySet()) {
+            boolean isBanned = false;
+            for (String ban : banned) {
+                if (!key.equals(ban)) {
+                    if (map.get(key) > max) {
+                        max = map.get(key);
+                        answer = key;
+                    }
+                } else {
+                    isBanned = true;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
     public String mostCommonWord(String paragraph, String[] banned) {
         // Get rid of all special characters and turn string to lowercase letters only
         paragraph = paragraph.replaceAll("[^a-zA-Z ]", " ").toLowerCase();
