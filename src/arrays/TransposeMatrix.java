e7c877223fc6137abbbe44002938c0b8ad7ee39d
buc0025 <stanley.yang001@gmail.com>
2022-09-10 21:06:45 -0400

    Leetcode 867 Transpose Matrix

--- src/arrays/TransposeMatrix.java
+++ src/arrays/TransposeMatrix.java
@@ -8,6 +8,23 @@ The transpose of a matrix is the matrix flipped over its main diagonal, switchin
 
 public class TransposeMatrix {
 
+    // 8/30/2022 solution
+    public int[][] transpose(int[][] matrix) {
+        int[][] arr = new int[matrix[0].length][matrix.length];
+        int col = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            int row = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                arr[row][col] = matrix[i][j];
+                row++;
+            }
+            col++;
+        }
+
+        return arr;
+    }
+
     public int[][] transpose(int[][] matrix) {
         int[][] arr = new int[matrix[0].length][matrix.length];
 
