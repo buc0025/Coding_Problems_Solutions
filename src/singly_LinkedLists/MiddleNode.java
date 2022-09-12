29f680a0b1c395445f11e2074bb2a03224b8861a
buc0025 <stanley.yang001@gmail.com>
2022-09-12 19:47:16 -0400

    Leetcode 876 Middle of the Linked List

--- src/singly_LinkedLists/MiddleNode.java
+++ src/singly_LinkedLists/MiddleNode.java
@@ -8,6 +8,23 @@ If there are two middle nodes, return the second middle node.
 
 public class MiddleNode {
 
+    // 9/12/2022 solution
+    public ListNode middleNode(ListNode head) {
+        ListNode current = head;
+        int len = 0;
+
+        while (current != null) {
+            current = current.next;
+            len++;
+        }
+
+        for (int i = 0; i < len/2; i++) {
+            head = head.next;
+        }
+
+        return head;
+    }
+
     public ListNode middleNode(ListNode head) {
 
         ListNode fast = head;
