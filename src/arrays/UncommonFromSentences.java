038aaad757f2c9224fed59ef1ba3e05f0c38ad51
buc0025 <stanley.yang001@gmail.com>
2022-09-13 20:36:51 -0400

    Leetcode 884 Uncommon Words from Two Sentences

--- src/arrays/UncommonFromSentences.java
+++ src/arrays/UncommonFromSentences.java
@@ -10,6 +10,42 @@ You may return the list in any order.
 
 public class UncommonFromSentences {
 
+    // 9/13/2022 solution
+    public String[] uncommonFromSentences(String s1, String s2) {
+        String combo = s1 + " " + s2;
+        String[] words = combo.split(" ");
+
+        Map<String, Integer> map = new HashMap();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+
+        for (String word : map.keySet()) {
+            if (map.get(word) == 1) {
+                list.add(word);
+            }
+        }
+
+        String[] ans = new String[list.size()];
+        int index = 0;
+
+        for (String word : map.keySet()) {
+            if (map.get(word) == 1) {
+                ans[index] = word;
+                index++;
+            }
+        }
+
+        return ans;
+    }
+
     public String[] uncommonFromSentences(String A, String B) {
         List<String> list = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
