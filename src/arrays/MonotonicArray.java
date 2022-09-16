d6318c4e1d5d2c739130083c11d8dbd6d465ebb1
buc0025 <stanley.yang001@gmail.com>
2022-09-15 20:30:21 -0400

    Leetcode 896 Monotonic Array

--- src/arrays/MonotonicArray.java
+++ src/arrays/MonotonicArray.java
@@ -11,6 +11,32 @@ Return true if and only if the given array A is monotonic.
 
 public class MonotonicArray {
 
+    // 9/15/2022 solution
+    public boolean isMonotonic(int[] nums) {
+        if (nums[0] > nums[nums.length - 1]) {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] > nums[i - 1]) {
+                    return false;
+                }
+            }
+            return true;
+        } else if (nums[0] < nums[nums.length - 1]) {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[i - 1]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i - 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
     public boolean isMonotonic(int[] arr) {
         int max = arr[0];
         int min = arr[0];
