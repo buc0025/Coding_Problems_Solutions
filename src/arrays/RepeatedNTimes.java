a58ad4bfadafffda4b498d51eb466870fd36d258
buc0025 <stanley.yang001@gmail.com>
2022-10-07 20:30:53 -0400

    Leetcode 961 N-Repeated Element in Size 2N Array

--- src/arrays/RepeatedNTimes.java
+++ src/arrays/RepeatedNTimes.java
@@ -8,6 +8,46 @@ Return the element repeated N times.
 
 public class RepeatedNTimes {
 
+    // 10/7/2022 solution
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length / 2;
+        int[] arr = new int[10000];
+
+        for (int i = 0; i < nums.length; i++) {
+            arr[nums[i]]++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    // 10/7/2022 solution
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length / 2;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (int key : map.keySet()) {
+            if (map.get(key) == n) {
+                return key;
+            }
+        }
+
+        return 0;
+    }
+
     public int repeatedNTimes(int[] arr) {
         int n = arr.length/2;
         int ans = -1;
