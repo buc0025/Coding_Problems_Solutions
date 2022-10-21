07be3c6f944336d3f86891dd502caa09a7231874
buc0025 <stanley.yang001@gmail.com>
2022-10-20 21:30:49 -0400

    Leetcode 32 Search Insert Position

--- src/arrays/SearchInsertPosition.java
+++ src/arrays/SearchInsertPosition.java
@@ -7,6 +7,27 @@ where it would be if it were inserted in order.
 
 public class SearchInsertPosition {
 
+    // 10/18/2022 solution
+    public int searchInsert(int[] nums, int target) {
+        if (target < nums[0]) {
+            return 0;
+        }
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+            if (nums[i] > target) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
     public int searchInsert(int[] nums, int target) {
         int ans = -1;
         // Iteration is if target is found in array
